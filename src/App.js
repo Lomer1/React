f1a2504a5c29353ad1b75bfb5012ace9d1116fda
Lorem1 <smolen433yandex.ru>
2022-09-17 13:14:28 +0300

    remove_post

--- src/App.js
+++ src/App.js
@@ -21,12 +21,15 @@ function App() {
     setPosts([...posts, newPost])
   }
 
+  const removePost = (post) => {
+    setPosts(posts.filter(p => p.id !== post.id ))
+  }
 
 
   return (
     <div className="App">
       <PostForm create = {createPost}/>
-      <PostList posts = {posts} title = "Список постов 1"/>
+      <PostList remove = {removePost} posts = {posts} title = "Список постов 1"/>
     </div>
   );
 }
